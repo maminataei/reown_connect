66c10205440afaab08404da87bf0b73a9c9958c4
Amin Ataei ⚡ <maminataee@gmail.com>
2025-03-19 03:14:52 +0330

    absolute imports configured

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@routes": "/src/routes",
+      "@store": "/src/store",
+      "@utils": "/src/utils",
+    },
+  },
 });
